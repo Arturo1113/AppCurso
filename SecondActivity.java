a8727614384ac5149f4f55306a929d9660328255
Laybors <bobeka0306@gmail.com>
2017-08-02 00:54:31 -0500

    Update SecondActivity.java

--- SecondActivity.java
+++ SecondActivity.java
@@ -2,13 +2,25 @@ package ejemplo.pruebafinal;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity
+{
+
+    TextView nameText2;
+    TextView ageText2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        nameText2 = (TextView) findViewById(R.id.name);
+        ageText2 = (TextView) findViewById(R.id.age);
+
+        nameText2.setText(getIntent().getStringExtra("NAME"));
+        ageText2.setText(getIntent().getStringExtra("AGE"));
     }
 
 }
