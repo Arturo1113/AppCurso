4163c4d506d5b8e21b8a442e233d55b01f09b79b
Laybors <bobeka0306@gmail.com>
2017-08-02 01:05:27 -0500

    Update SecondActivity.java

--- SecondActivity.java
+++ SecondActivity.java
@@ -1,7 +1,10 @@
 package ejemplo.pruebafinal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity
@@ -9,6 +12,8 @@ public class SecondActivity extends AppCompatActivity
 
     TextView nameText2;
     TextView ageText2;
+    Button backButton;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,9 +23,20 @@ public class SecondActivity extends AppCompatActivity
 
         nameText2 = (TextView) findViewById(R.id.name);
         ageText2 = (TextView) findViewById(R.id.age);
+        backButton = (Button) findViewById(R.id.back);
 
         nameText2.setText(getIntent().getStringExtra("NAME"));
         ageText2.setText(getIntent().getStringExtra("AGE"));
-    }
 
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                intent = new Intent(SecondActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
 }
